f7d0c496ce2a5c87fcbdeb9cf89b91729a4a7f25
Olimhon Mamatkulov <mamatkulov@webmoney.ru>
2018-05-16 17:36:14 +0300

    Added version 1.1

--- src/digiseller/digiseller.php
+++ src/digiseller/digiseller.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package Digiseller
- * @version 0.1
+ * @version 1.1
  */
 /*
 Plugin Name: Digiseller
 Plugin URI: https://github.com/digiseller/wordpress-plugin
 Description: Easy integration of http://digiseller.ru into your website.
 Author: Digiseller
-Version: 1.0.0
+Version: 1.1.0
 Author URI: http://digiseller.ru
 Text Domain: digiseller
 Domain Path: /languages
@@ -19,7 +19,7 @@ defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
 
 add_action('plugins_loaded', 'digiseller_init');
 function digiseller_init() {
-	load_plugin_textdomain( 'digiseller', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
+    load_plugin_textdomain( 'digiseller', FALSE, basename( dirname( __FILE__ ) ) . '/languages/' );
 }
 
 if (!is_admin())
@@ -27,78 +27,94 @@ add_shortcode( 'ds', 'digiseller_shortcode' );
 
 function digiseller_shortcode( $atts )
 {
-	$r = '';
+    $r = '';
 
-	if (!isset($atts['id']) or empty($atts['id'])) {
-		$r.= "\n\n";
-		$r.= '<!--[ds]-->';
-		$r.= "\n\n";
-		$r.= '<span class="error">[ds ' . __('attribute id is not set', 'digiseller') . ']</span>';
-		$r.= "\n\n";
-		$r.= '<!--[/ds]-->';
-		$r.= "\n\n";
-		return $r;
-	}
+    if (!isset($atts['id']) or empty($atts['id'])) {
+        $r.= "\n\n";
+        $r.= '<!--[ds]-->';
+        $r.= "\n\n";
+        $r.= '<span class="error">[ds ' . __('attribute id is not set', 'digiseller') . ']</span>';
+        $r.= "\n\n";
+        $r.= '<!--[/ds]-->';
+        $r.= "\n\n";
+        return $r;
+    }
 
-	$id = $atts['id'];
-	$op = (isset($atts['op'])) ? $atts['op'] : 'logo,search,cart,purchases,lang,cath,menu';
-
-
-	$options = array(
-		'sellerid' => $id,
-		'logo' => strpos($op, 'logo') !== false,
-		'search' => strpos($op, 'search') !== false,
-		'cart' => strpos($op, 'cart') !== false,
-		'purchases' => strpos($op, 'purchases') !== false,
-		'lang' => strpos($op, 'lang') !== false,
-		'cat' => strpos($op, 'catv') !== false ? 'v' : strpos($op, 'cat') !== false ? 'h' : '0',
-		'menu' => strpos($op, 'menu') !== false,
-	);
+    $id = $atts['id'];
+    $op = (isset($atts['op'])) ? $atts['op'] : 'logo,search,cart,purchases,lang,cath,menu,widen';
 
 
-	$r.= "\n\n";
-	$r.= '<!--[ds id="' . $id . '" op="' . $op . '"]-->';
-	$r.= "\n\n";
-	$r.= digiseller_generator($options);
-	$r.= "\n\n";
-	$r.= '<!--[/ds id="' . $id . '" op="' . $op . '"]-->';
-	$r.= "\n\n";
-	return $r;
+    $options = array(
+        'sellerid' => $id,
+        'logo' => strpos($op, 'logo') !== false,
+        'search' => strpos($op, 'search') !== false,
+        'cart' => strpos($op, 'cart') !== false,
+        'purchases' => strpos($op, 'purchases') !== false,
+        'lang' => strpos($op, 'lang') !== false,
+        'cat' => strpos($op, 'catv') !== false ? 'v' : strpos($op, 'cat') !== false ? 'h' : '0',
+        'menu' => strpos($op, 'menu') !== false,
+        'widen' => strpos($op, 'widen') !== false,
+    );
+
+
+    $r.= "\n\n";
+    $r.= '<!--[ds id="' . $id . '" op="' . $op . '"]-->';
+    $r.= "\n\n";
+    $r.= digiseller_generator($options);
+    $r.= "\n\n";
+    $r.= '<!--[/ds id="' . $id . '" op="' . $op . '"]-->';
+    $r.= "\n\n";
+    return $r;
 }
 
 
 
 function digiseller_generator($options) {
 
-	$o = array(
-		'sellerid' => $options['sellerid'] ? 1 : 0,
-		'logo' => $options['logo'] ? 1 : 0,
-		'search' => $options['search'] ? 1 : 0,
-		'cart' => $options['cart'] ? 1 : 0,
-		'purchases' => $options['purchases'] ? 1 : 0,
-		'lang' => $options['lang'] ? 1 : 0,
-		'cat' => in_array($options['cat'], array('h', 'v')) ? $options['cat'] : '0',
-		'menu' => $options['menu'] ? 1 : 0,
-	);
+    $o = array(
+        'logo' => $options['logo'] ? 1 : 0,
+        'search' => $options['search'] ? 1 : 0,
+        'cart' => $options['cart'] ? 1 : 0,
+        'purchases' => $options['purchases'] ? 1 : 0,
+        'lang' => $options['lang'] ? 1 : 0,
+        'cat' => in_array($options['cat'], array('h', 'v')) ? $options['cat'] : '0',
+        'menu' => $options['menu'] ? 1 : 0,
+    );
 
-	ob_start();
-	?>
+    ob_start();
+    ?>
 
-	<script>!function(e){var l=function(l){return e.cookie.match(new RegExp("(?:^|; )digiseller-"+l+"=([^;]*)"))},i=l("lang"),s=l("cart_uid"),t=i?"&lang="+i[1]:"",d=s?"&cart_uid="+s[1]:"",r=e.getElementsByTagName("head")[0]||e.documentElement,n=e.createElement("link"),a=e.createElement("script");n.type="text/css",n.rel="stylesheet",n.id="digiseller-css",n.href="//shop.digiseller.ru/xml/store_css.asp?seller_id=<?php echo $options['sellerid']; ?>",a.async=!0,a.id="digiseller-js",a.src="//www.digiseller.ru/store/digiseller-api.js.asp?seller_id=<?php echo $options['sellerid']; ?>"+t+d,!e.getElementById(n.id)&&r.appendChild(n),!e.getElementById(a.id)&&r.appendChild(a)}(document);</script>
+    <?php if ($options['widen']): ?>
+        <style>
+            @media screen and (min-width: 48em) {
+                .blog:not(.has-sidebar) #primary article,
+                .archive:not(.page-one-column):not(.has-sidebar) #primary article,
+                .search:not(.has-sidebar) #primary article,
+                .error404:not(.has-sidebar) #primary .page-content,
+                .error404.has-sidebar #primary .page-content,
+                body.page-two-column:not(.archive) #primary .entry-content,
+                body.page-two-column #comments {
+                       width: 100%;
+                }
+            }
+        </style>
+    <?php endif; ?>
 
-	<span class="digiseller-body" id="digiseller-body" 
-		data-logo="<?php echo $o['logo']; ?>" 
-		data-search="<?php echo $o['search']; ?>"
-		data-cart="<?php echo $o['cart']; ?>" 
-		data-purchases="<?php echo $o['purchases']; ?>" 
-		data-langs="<?php echo $o['lang']; ?>" 
-		data-cat="<?php echo $o['cat']; ?>" 
-		data-downmenu="<?php echo $o['menu']; ?>"></span>
+    <script>!function(e){var l=function(l){return e.cookie.match(new RegExp("(?:^|; )digiseller-"+l+"=([^;]*)"))},i=l("lang"),s=l("cart_uid"),t=i?"&lang="+i[1]:"",d=s?"&cart_uid="+s[1]:"",r=e.getElementsByTagName("head")[0]||e.documentElement,n=e.createElement("link"),a=e.createElement("script");n.type="text/css",n.rel="stylesheet",n.id="digiseller-css",n.href="//shop.digiseller.ru/xml/store2_css.asp?seller_id=<?php echo $options['sellerid']; ?>",a.async=!0,a.id="digiseller-js",a.src="//www.digiseller.ru/store2/digiseller-api.js.asp?seller_id=<?php echo $options['sellerid']; ?>"+t+d,!e.getElementById(n.id)&&r.appendChild(n),!e.getElementById(a.id)&&r.appendChild(a)}(document);</script>
 
-	<?php
-	$r = ob_get_contents();
-	ob_end_clean();
-	return $r;
+    <span class="digiseller-body" id="digiseller-body" 
+        data-logo="<?php echo $o['logo']; ?>" 
+        data-search="<?php echo $o['search']; ?>"
+        data-cart="<?php echo $o['cart']; ?>" 
+        data-purchases="<?php echo $o['purchases']; ?>" 
+        data-langs="<?php echo $o['lang']; ?>" 
+        data-cat="<?php echo $o['cat']; ?>" 
+        data-downmenu="<?php echo $o['menu']; ?>"></span>
+
+    <?php
+    $r = ob_get_contents();
+    ob_end_clean();
+    return $r;
 }
 
 
@@ -110,24 +126,25 @@ add_filter( 'the_content', 'digiseller_content' );
 
 function digiseller_content( $content ) {
 
-	$pageid = get_option( 'digiseller_setting_pageid', 0 );
-	$sellerid = get_option( 'digiseller_setting_sellerid', 0 );
+    $pageid = get_option( 'digiseller_setting_pageid', 0 );
+    $sellerid = get_option( 'digiseller_setting_sellerid', 0 );
 
-	if ( !empty($pageid) and !empty($sellerid) and is_page( $pageid ) ) {
-		$options = array(
-			'sellerid' => $sellerid,
-			'logo' => get_option( 'digiseller_setting_logo', 'off' ) == 'on',
-			'search' => get_option( 'digiseller_setting_search', 'off' ) == 'on',
-			'cart' => get_option( 'digiseller_setting_cart', 'off' ) == 'on',
-			'purchases' => get_option( 'digiseller_setting_purchases', 'off' ) == 'on',
-			'lang' => get_option( 'digiseller_setting_lang', 'off' ) == 'on',
-			'cat' => get_option( 'digiseller_setting_cat', '0' ),
-			'menu' => get_option( 'digiseller_setting_menu', 'off' ) == 'on',
-		);
+    if ( !empty($pageid) and !empty($sellerid) and is_page( $pageid ) ) {
+        $options = array(
+            'sellerid' => $sellerid,
+            'logo' => get_option( 'digiseller_setting_logo', 'off' ) == 'on',
+            'search' => get_option( 'digiseller_setting_search', 'off' ) == 'on',
+            'cart' => get_option( 'digiseller_setting_cart', 'off' ) == 'on',
+            'purchases' => get_option( 'digiseller_setting_purchases', 'off' ) == 'on',
+            'lang' => get_option( 'digiseller_setting_lang', 'off' ) == 'on',
+            'cat' => get_option( 'digiseller_setting_cat', '0' ),
+            'menu' => get_option( 'digiseller_setting_menu', 'off' ) == 'on',
+            'widen' => get_option( 'digiseller_setting_widen', 'off' ) == 'on',
+        );
 
-		return digiseller_generator($options);
-	}
-	return $content;
+        return digiseller_generator($options);
+    }
+    return $content;
 }
 
 
@@ -148,7 +165,7 @@ function digiseller_settings_admin_menu() {
 
 function digiseller_settings_page() {
     //if ( isset( $_REQUEST['settings-updated'] ) ) { echo '<pre>'; print_r($_REQUEST); die; }    
-	?>
+    ?>
     <div class="wrap">
  
         <h2><?php echo esc_html( get_admin_page_title() ); ?></h2>
@@ -157,13 +174,21 @@ function digiseller_settings_page() {
             <?php settings_fields( 'digiseller_settings' ); ?>
             <?php do_settings_sections( 'digiseller_settings' ); ?>
 
-            <div class="info">* <?php _e('Menu items can be configured in', 'digiseller'); ?> <a href="https://my.digiseller.ru/inside/myshop.asp?view=settings"><?php _e('settings', 'digiseller'); ?></a></div>
-			<div class="info">** <?php _e("Only your products can be added into cart (not partners' products)", 'digiseller'); ?></div>
+            <div class="info">
+                * <?php echo __("Only your products can be added into cart (not partners' products)", 'digiseller'); ?>
+            </div>
+            <div class="info">
+                ** <?php echo __('Menu items can be configured in', 'digiseller'); ?>
+                <a href="https://my.digiseller.ru/inside/myshop.asp?view=settings"><?php echo __('settings', 'digiseller'); ?></a>
+            </div>
+            <div class="info">
+                *** <?php echo __('Overwrites some default styles of wordpress theme', 'digiseller'); ?>
+            </div>
 
             <?php submit_button(); ?>
         </form>
     </div>
-	<?php
+    <?php
 }
 
 
@@ -175,82 +200,88 @@ add_action( 'admin_init', 'digiseller_settings' );
 
 function digiseller_settings() {
 
-	add_settings_section(
-		'digiseller_setting_section',
-		__('Select what to show on site', 'digiseller'),
-		'digiseller_setting_section_callback',
-		'digiseller_settings'
-	);
-	 
-	add_settings_field(
-		'digiseller_setting_pageid', __('Page', 'digiseller'),
-		'digiseller_setting_pageid_callback', 'digiseller_settings', 'digiseller_setting_section'
-	);
+    add_settings_section(
+        'digiseller_setting_section',
+        __('Select what to show on site', 'digiseller'),
+        'digiseller_setting_section_callback',
+        'digiseller_settings'
+    );
+     
+    add_settings_field(
+        'digiseller_setting_pageid', __('Page', 'digiseller'),
+        'digiseller_setting_pageid_callback', 'digiseller_settings', 'digiseller_setting_section'
+    );
 
-	add_settings_field(
-		'digiseller_setting_sellerid', __('SellerID', 'digiseller'),
-		'digiseller_setting_sellerid_callback', 'digiseller_settings', 'digiseller_setting_section'
-	);
+    add_settings_field(
+        'digiseller_setting_sellerid', __('SellerID', 'digiseller'),
+        'digiseller_setting_sellerid_callback', 'digiseller_settings', 'digiseller_setting_section'
+    );
 
-	add_settings_field(
-		'digiseller_setting_logo', __('Logo', 'digiseller'),
-		'digiseller_setting_logo_callback', 'digiseller_settings', 'digiseller_setting_section'
-	);
+    add_settings_field(
+        'digiseller_setting_logo', __('Logo', 'digiseller'),
+        'digiseller_setting_logo_callback', 'digiseller_settings', 'digiseller_setting_section'
+    );
 
-	add_settings_field(
-		'digiseller_setting_search', __('Search', 'digiseller'),
-		'digiseller_setting_search_callback', 'digiseller_settings', 'digiseller_setting_section'
-	);
+    add_settings_field(
+        'digiseller_setting_search', __('Search', 'digiseller'),
+        'digiseller_setting_search_callback', 'digiseller_settings', 'digiseller_setting_section'
+    );
 
-	add_settings_field(
-		'digiseller_setting_cart', __('Cart', 'digiseller') . ' **',
-		'digiseller_setting_cart_callback', 'digiseller_settings', 'digiseller_setting_section'
-	);
+    add_settings_field(
+        'digiseller_setting_cart', __('Cart', 'digiseller') . ' *',
+        'digiseller_setting_cart_callback', 'digiseller_settings', 'digiseller_setting_section'
+    );
 
-	add_settings_field(
-		'digiseller_setting_purchases', __('Purchases', 'digiseller'),
-		'digiseller_setting_purchases_callback', 'digiseller_settings', 'digiseller_setting_section'
-	);
+    add_settings_field(
+        'digiseller_setting_purchases', __('Purchases', 'digiseller'),
+        'digiseller_setting_purchases_callback', 'digiseller_settings', 'digiseller_setting_section'
+    );
 
-	add_settings_field(
-		'digiseller_setting_lang', __('Language selector', 'digiseller'),
-		'digiseller_setting_lang_callback', 'digiseller_settings', 'digiseller_setting_section'
-	);
+    add_settings_field(
+        'digiseller_setting_lang', __('Language selector', 'digiseller'),
+        'digiseller_setting_lang_callback', 'digiseller_settings', 'digiseller_setting_section'
+    );
 
-	add_settings_field(
-		'digiseller_setting_cat', __('Categories', 'digiseller'),
-		'digiseller_setting_cat_callback', 'digiseller_settings', 'digiseller_setting_section'
-	);
+    add_settings_field(
+        'digiseller_setting_cat', __('Categories', 'digiseller'),
+        'digiseller_setting_cat_callback', 'digiseller_settings', 'digiseller_setting_section'
+    );
 
-	add_settings_field(
-		'digiseller_setting_menu', __('Bottom menu', 'digiseller') . ' *',
-		'digiseller_setting_menu_callback', 'digiseller_settings', 'digiseller_setting_section'
-	);
+    add_settings_field(
+        'digiseller_setting_menu', __('Bottom menu', 'digiseller') . ' **',
+        'digiseller_setting_menu_callback', 'digiseller_settings', 'digiseller_setting_section'
+    );
 
-	register_setting( 'digiseller_settings', 'digiseller_setting_pageid' );
-	register_setting( 'digiseller_settings', 'digiseller_setting_sellerid' );
-	register_setting( 'digiseller_settings', 'digiseller_setting_logo' );
-	register_setting( 'digiseller_settings', 'digiseller_setting_search' );
-	register_setting( 'digiseller_settings', 'digiseller_setting_cart' );
-	register_setting( 'digiseller_settings', 'digiseller_setting_purchases' );
-	register_setting( 'digiseller_settings', 'digiseller_setting_lang' );
-	register_setting( 'digiseller_settings', 'digiseller_setting_cat' );
-	register_setting( 'digiseller_settings', 'digiseller_setting_menu' );
+    add_settings_field(
+        'digiseller_setting_widen', __('Output to full page width', 'digiseller') . ' ***',
+        'digiseller_setting_widen_callback', 'digiseller_settings', 'digiseller_setting_section'
+    );
+
+    register_setting( 'digiseller_settings', 'digiseller_setting_pageid' );
+    register_setting( 'digiseller_settings', 'digiseller_setting_sellerid' );
+    register_setting( 'digiseller_settings', 'digiseller_setting_logo' );
+    register_setting( 'digiseller_settings', 'digiseller_setting_search' );
+    register_setting( 'digiseller_settings', 'digiseller_setting_cart' );
+    register_setting( 'digiseller_settings', 'digiseller_setting_purchases' );
+    register_setting( 'digiseller_settings', 'digiseller_setting_lang' );
+    register_setting( 'digiseller_settings', 'digiseller_setting_cat' );
+    register_setting( 'digiseller_settings', 'digiseller_setting_menu' );
+    register_setting( 'digiseller_settings', 'digiseller_setting_widen' );
 }
  
 function digiseller_setting_section_callback() {
-	//echo '<p>DigiSeller settings</p>';
+    //echo '<p>DigiSeller settings</p>';
 }
 
 function digiseller_setting_pageid_callback() {
-	$option = 'digiseller_setting_pageid';
-	wp_dropdown_pages( array( 
-		'id' => $option, 
-		'name' => $option, 
-		'show_option_none' => __( '&mdash; Select &mdash;' ), 
-		'option_none_value' => '0', 
-		'selected' => get_option( $option ),
-	) );
+    $option = 'digiseller_setting_pageid';
+    wp_dropdown_pages( array( 
+        'id' => $option, 
+        'name' => $option, 
+        'show_option_none' => __( '&mdash; Select &mdash;' ), 
+        'option_none_value' => '0', 
+        'selected' => get_option( $option ),
+    ) );
 }
 
 function digiseller_setting_sellerid_callback() {
@@ -264,7 +295,7 @@ function digiseller_setting_cart_callback() { digiseller_setting_checkbox_callba
 function digiseller_setting_purchases_callback() { digiseller_setting_checkbox_callback( 'digiseller_setting_purchases' ); }
 function digiseller_setting_lang_callback() { digiseller_setting_checkbox_callback( 'digiseller_setting_lang' ); }
 function digiseller_setting_cat_callback() { 
-	$cat = get_option( 'digiseller_setting_cat', '0' );
+    $cat = get_option( 'digiseller_setting_cat', '0' );
     echo '<select  id="digiseller_setting_cat" name="digiseller_setting_cat">';
     echo '    <option value="0"' . (($cat == '0') ? ' selected' : '') . '>' . __('Hide', 'digiseller') . '</option>';
     echo '    <option value="h"' . (($cat == 'h') ? ' selected' : '') . '>' . __('Horizontal', 'digiseller') . '</option>';
@@ -278,7 +309,7 @@ function digiseller_setting_checkbox_callback( $option ) {
     echo '<input type="checkbox" id="' . $option . '" name="' . $option . '"' . ( ( get_option( $option ) == 'on' ) ? ' checked="checked"' : '' ) . ' />';
 }
 
-
+function digiseller_setting_widen_callback() { digiseller_setting_checkbox_callback( 'digiseller_setting_widen' ); }
 
 
 
--- src/digiseller/readme.txt
+++ src/digiseller/readme.txt
@@ -2,7 +2,7 @@
 Contributors: digiseller
 Tags: digiseller, shop, commerce, sell
 Requires at least: 3.0.1
-Tested up to: 4.3.1
+Tested up to: 4.9.5
 Stable tag: trunk
 License: GPLv2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
@@ -12,6 +12,7 @@ Easy integration of http://digiseller.ru into your website.
 == Description ==
 
 Easy integration of http://digiseller.ru into your website.
+[Installation documentation with screenshots.](https://docs.google.com/document/d/1oaKI6tHKOYEwWZ9k-vgBC3sGYskKkuZ74amQ-RzhQEA/)
 
 == Installation ==
 
@@ -19,4 +20,4 @@ Easy integration of http://digiseller.ru into your website.
 1. Activate the plugin through the 'Plugins' menu in WordPress
 1.
  * Configure from 'Settins->Digiseller' menu in WordPress
- * Use short code `[ds id="xxx" op="logo,search,cart,purchases,lang,cath,menu"]` in your conent
+ * Use short code `[ds id="xxx" op="logo,search,cart,purchases,lang,cath,menu,widen"]` in your content
